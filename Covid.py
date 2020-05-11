7141d36cb3b02ea1d8930c123104b13abf862abe
bjhans <bjhans@kth.se>
2020-05-11 13:41:31 +0200

    added tensorflow-crap

---
+++ Covid.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # REPLACE WITH COVID SHIT
+
+
+class_names = ['covid', 'not covid']
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])
+
+
+if __name__ == '__main__':
+    print ('test')
+
