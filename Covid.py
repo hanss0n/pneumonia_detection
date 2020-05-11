8aec380fdfebff2bf102d4bfda2dd7920a4d18f9
bjhans <bjhans@kth.se>
2020-05-11 13:53:51 +0200

    added tensorflow-crap

--- Covid.py
+++ Covid.py
@@ -3,13 +3,33 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 fashion_mnist = keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # REPLACE WITH COVID SHIT
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # REPLACE WITH COVID SHIT
 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-class_names = ['covid', 'not covid']
+# preprocess data
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+# scale values to 0 and 1 before feeding to neural network
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
@@ -17,7 +37,18 @@ model = keras.Sequential([
     keras.layers.Dense(10)
 ])
 
+model.compile(optimizer='adam',
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=1)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+probability_model = keras.Sequential([model,
+                                      keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
 
 if __name__ == '__main__':
-    print ('test')
-
+    print('test')
