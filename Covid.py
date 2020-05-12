518710cbccb5bd1bf3d192b5a70385ffee655bba
felixlut <felixlut@gmail.com>
2020-05-12 16:32:04 +0200

    Restructure code to make additions easier

--- Covid.py
+++ Covid.py
@@ -3,52 +3,43 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-fashion_mnist = keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # REPLACE WITH COVID SHIT
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', # Replace with covid, notcovid
+def load_tutorial_data():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # REPLACE WITH COVID SHIT
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', # Replace with covid, notcovid
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    return train_images, train_labels, test_images, test_labels, class_names
 
-# preprocess data 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
 
-# scale values to 0 and 1 before feeding to neural network
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+def preprocess_data(train_images, train_labels, test_images, test_labels, class_names):
+    # scale values to 0 and 1 before feeding to neural network
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
 
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(10)
+    ])
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10)
-])
+    model.compile(optimizer='adam',
+                  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
 
-model.compile(optimizer='adam',
-              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+    model.fit(train_images, train_labels, epochs=0) # TODO: Fix before commit
 
-model.fit(train_images, train_labels, epochs=5)
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+    probability_model = keras.Sequential([model,
+                                          keras.layers.Softmax()])
 
-probability_model = keras.Sequential([model,
-                                      keras.layers.Softmax()])
+    predictions = probability_model.predict(test_images)
 
-predictions = probability_model.predict(test_images)
+
+
 
 if __name__ == '__main__':
-    print('test')
+    train_images, train_labels, test_images, test_labels, class_names = load_tutorial_data()
+    preprocess_data(train_images, train_labels, test_images, test_labels, class_names)+
+
