5af5d5b1de10394a419bc6d7bd8c9e7120852c55
felixlut <felixlut@gmail.com>
2020-05-13 17:28:37 +0200

    Early draft of the network, alot of fixes to be done, but it runs

--- Covid.py
+++ Covid.py
@@ -11,8 +11,14 @@ import pandas as pd
 import os
 from PIL import Image
 import re
-import shutil
-import sys
+
+
+train_dir = os.path.join('res', 'train')
+validation_dir = os.path.join('res', 'validation')
+train_covid_dir = os.path.join(train_dir, 'covid')
+train_non_covid_dir = os.path.join(train_dir, 'non_covid')
+validation_covid_dir = os.path.join(validation_dir, 'covid')
+validation_non_covid_dir = os.path.join(validation_dir, 'non_covid')
 
 
 def load_tutorial_data():
@@ -89,6 +95,108 @@ def load_data():
 
     return train_images, train_labels, test_images, test_labels, class_names
 
+
+def actual_stuff():
+    # Give an overview of what data we have
+    num_covid_tr = len(os.listdir(train_covid_dir))
+    num_non_covid_tr = len(os.listdir(train_non_covid_dir))
+
+    num_covid_val = len(os.listdir(validation_covid_dir))
+    num_non_covid_val = len(os.listdir(validation_non_covid_dir))
+
+    total_train = num_covid_tr + num_non_covid_tr
+    total_val = num_covid_val + num_non_covid_val
+
+    print('total training covid images:', num_covid_tr)
+    print('total training non_covid images:', num_non_covid_tr)
+
+    print('total validation covid images:', num_covid_val)
+    print('total validation non_covid images:', num_non_covid_val)
+    print('-----------------------------------------------')
+    print("Total training images:", total_train)
+    print("Total validation images:", total_val)
+
+    # Define parameters for our network
+    batch_size = 10
+    epochs = 15
+    IMG_HEIGHT = 150
+    IMG_WIDTH = 150
+
+    # Do some rescaling
+    # TODO: Will this work with a combination of grayscale/RGB/RGBA??????????
+    train_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator for our training data
+    validation_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator for our validation data
+
+    train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                               directory=train_dir,
+                                                               shuffle=True,
+                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                               class_mode='binary')
+
+    val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+                                                                  directory=validation_dir,
+                                                                  target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                                  class_mode='binary')
+
+    sample_training_images, _ = next(train_data_gen)
+
+    model = Sequential([
+        Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+        MaxPooling2D(),
+        Conv2D(32, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Conv2D(64, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Flatten(),
+        Dense(512, activation='relu'),
+        Dense(1)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    history = model.fit_generator(
+        train_data_gen,
+        steps_per_epoch=total_train // batch_size,
+        epochs=epochs,
+        validation_data=val_data_gen,
+        validation_steps=total_val // batch_size
+    )
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
 def count_color_modes(images):
     grey_scale, rgb, rgba, i = 0, 0, 0, 0
     for img in images:
@@ -110,5 +218,10 @@ if __name__ == '__main__':
     # train_images, train_labels, test_images, test_labels, class_names = load_data()
     # print(sys.getsizeof(train_images))
     # preprocess_data(train_images, train_labels, test_images, test_labels, class_names)
-    tutorial2()
+    actual_stuff()
+
+
+
+
+
 
