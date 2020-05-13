79059bffe453a4ba67869cfa14ec4b55afb73e97
felixlut <felixlut@gmail.com>
2020-05-13 12:44:07 +0200

    Also store non-covid cases from the chestxray dataset

--- res/dataset_handler.py
+++ res/dataset_handler.py
@@ -9,6 +9,7 @@ def extract_covid_chestxray_dataset():
     working_dir = 'covid-chestxray-dataset'
     # Where we will store our extracted images
     covid_path = os.path.join(working_dir, 'covid_cases')
+    non_covid_path = os.path.join(working_dir, 'non_covid')
     # If it hasn't already been created, do so
     if not os.path.exists(covid_path):
         try:
@@ -20,13 +21,24 @@ def extract_covid_chestxray_dataset():
     else:
         print("Directory %s already exists" % covid_path)
 
+    if not os.path.exists(non_covid_path):
+        try:
+            os.mkdir(non_covid_path)
+        except OSError:
+            print("Creation of the directory %s failed" % non_covid_path)
+        else:
+            print("Successfully created the directory %s " % non_covid_path)
+    else:
+        print("Directory %s already exists" % non_covid_path)
+
     metadata_csv = pd.read_csv(os.path.join(working_dir, 'metadata.csv'),
                                usecols=['patientid', 'finding', 'date', 'filename', 'view', 'folder'])
     metadata_csv.insert(0, 'New_ID', range(0, len(metadata_csv)))  # Adds unique row identifier
 
-    # Extract only Covid cases, as well as counting skipped and covid cases
-    covid_cases, skipped = 0, 0
-    res = []
+    # Extract only Covid cases, as well as counting non- and covid cases
+    covid_cases, non_covid_cases, skipped = 0, 0, 0
+    covid = []
+    non_covid = []
     for (i, row) in metadata_csv.iterrows():
         if re.match('images', row['folder']) and re.match('COVID-19', row['finding']):
             # Copy the Covid images to the directory storing all the covid cases
@@ -38,15 +50,24 @@ def extract_covid_chestxray_dataset():
 
             # Keep track of which images are saved
             covid_cases += 1
-            res.append(row['New_ID'])
+            covid.append(row['New_ID'])
         else:
-            skipped += 1
+            if re.match('volumes', row['folder']):
+                skipped += 1
+            else:
+                orig_path = os.path.join(working_dir, 'images', row["filename"])
+                if not os.path.exists(os.path.join(non_covid_path, row["filename"])):
+                    shutil.copy2(orig_path, non_covid_path)
+                non_covid_cases += 1
+                non_covid.append(row['New_ID'])
 
     print('Covid cases: ', covid_cases)
-    print('Skipped cases: ', skipped)
+    print('Non Covid cases: ', non_covid_cases)
+    print('Skipped: ', skipped)
 
     # Save an updated version of the metadata corresponding to the Covid cases
-    metadata_csv.loc[metadata_csv['New_ID'].isin(res)].to_csv(os.path.join(working_dir, 'metadata_covid_cases.csv'))
+    metadata_csv.loc[metadata_csv['New_ID'].isin(covid)].to_csv(os.path.join(working_dir, 'metadata_covid_cases.csv'))
+    metadata_csv.loc[metadata_csv['New_ID'].isin(non_covid)].to_csv(os.path.join(working_dir, 'metadata_non_covid.csv'))
 
 
 if __name__ == '__main__':
