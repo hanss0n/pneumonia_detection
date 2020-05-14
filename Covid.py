7049e555a6ff58654d9a389f723a481c0cd567d6
felixlut <felixlut@gmail.com>
2020-05-14 02:27:31 +0200

    Remove no longer used functions from Covid.py

--- Covid.py
+++ Covid.py
@@ -21,83 +21,6 @@ validation_covid_dir = os.path.join(validation_dir, 'NORMAL')
 validation_non_covid_dir = os.path.join(validation_dir, 'PNEUMONIA')
 
 
-def load_tutorial_data():
-    fashion_mnist = keras.datasets.fashion_mnist
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # REPLACE WITH COVID SHIT
-    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',  # Replace with covid_cases, notcovid
-                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    return train_images, train_labels, test_images, test_labels, class_names
-
-
-def preprocess_data(train_images, train_labels, test_images, test_labels, class_names):
-    # WARNING: The following lines will destroy your computer
-    # scale values to 0 and 1 before feeding to neural network
-    # for i in range(len(train_images)):
-    # train_images[i] = train_images[i] / 255.0
-    # print(i)
-    # train_images = [img / 255.0 for img in train_images]
-    # test_images = [img / 255.0 for img in test_images]
-
-    model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(28, 28)),
-        keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dense(10)
-    ])
-
-    model.compile(optimizer='adam',
-                  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-
-    model.fit(train_images, train_labels, epochs=0)  # TODO: Fix before commit
-
-    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-
-    probability_model = keras.Sequential([model,
-                                          keras.layers.Softmax()])
-
-    predictions = probability_model.predict(test_images)
-
-
-def load_data():
-    path_chestxray = os.path.join('res', 'covid-chestxray-dataset')
-    metadata_csv = pd.read_csv(os.path.join(path_chestxray, 'metadata.csv'),
-                               usecols=['patientid', 'finding', 'date', 'filename', 'view', 'folder'])
-
-    # Add unique row identifier
-    metadata_csv.insert(0, 'New_ID', range(0, len(metadata_csv)))
-
-    # Extract only images in the /images folder, as well as updating the metadata accordingly
-    covid_cases, skipped = 0, 0
-    usable_rows = []
-    for (i, row) in metadata_csv.iterrows():
-        if re.match('images', row['folder']):
-            covid_cases += 1
-            usable_rows.append(row['New_ID'])
-        else:
-            # There are 21 images in 'volume', we skip them for now
-            skipped += 1
-    # Update metadata
-    usable_metadata = metadata_csv.loc[metadata_csv['New_ID'].isin(usable_rows)]
-
-    # Store all the images we want to use
-    # We also have to transform our images to numpy arrays
-    images = [np.asarray(Image.open(os.path.join(path_chestxray, 'images', path))) for path in usable_metadata.filename]
-
-    # Extract all the labels for the images we are using
-    # Covid = 0, Not Covid = 1
-    labels = [0 if re.match('COVID-19', label) else 1 for label in usable_metadata.finding]
-
-    # TODO: Split these two into training + testing sets. For now it's a 50/50 split
-    half = int(len(usable_metadata) / 2)
-    train_images = images[:half]
-    train_labels = labels[:half]
-    test_images = images[half:]
-    test_labels = labels[half:]
-    class_names = ['COVID-19', 'NOT COVID-19']
-
-    return train_images, train_labels, test_images, test_labels, class_names
-
-
 def actual_stuff():
     # Give an overview of what data we have
     num_covid_tr = len(os.listdir(train_covid_dir))
@@ -254,8 +177,4 @@ def count_image_dims(images):
 
 
 if __name__ == '__main__':
-    # check_cats_and_dogs()
-    # train_images, train_labels, test_images, test_labels, class_names = load_data()
-    # print(sys.getsizeof(train_images))
-    # preprocess_data(train_images, train_labels, test_images, test_labels, class_names)
     actual_stuff()
