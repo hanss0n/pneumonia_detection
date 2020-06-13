de27a3de63ffc2b93dd73e7d5b790a6e22740ee3
felixlut <felixlut@gmail.com>
2020-06-13 16:06:19 +0200

    Add manual check before re-pulling kaggle dataset

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -1,11 +1,15 @@
 import kaggle
+import os
 
 
 def load_kaggle():
-    dataset = 'paultimothymooney/chest-xray-pneumonia'
-    target = 'kaggle'
-    kaggle.api.authenticate()
-    kaggle.api.dataset_download_files(dataset=dataset, path=target, unzip=True, quiet=False)
+    if not os.path.exists(os.path.join('kaggle', 'chest_xray')):
+        dataset = 'paultimothymooney/chest-xray-pneumonia'
+        target = 'kaggle'
+        kaggle.api.authenticate()
+        kaggle.api.dataset_download_files(dataset=dataset, path=target, unzip=True, quiet=False)
+    else:
+        print('The Kaggle dataset is already downloaded')
 
 
 if __name__ == '__main__':
